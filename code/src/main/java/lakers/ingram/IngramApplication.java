d2dfd33ba75e0ccdc501a21f3f522a4a5a027389
DESKTOP-1PVR0H2\z_bur <dumbledorebryant@sjtu.edu.cn>
2018-07-02 10:23:47 +0800

    basicConfig
    configuring

---
+++ code/src/main/java/lakers/ingram/IngramApplication.java
@@ -0,0 +1,12 @@
+package lakers.ingram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngramApplication.class, args);
+    }
+}
